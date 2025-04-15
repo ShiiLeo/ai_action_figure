ad0df21594988f3bd5adad48eee35382d3271b97
ShiiLeo <goodesign100@gmail.com>
2025-04-15 17:51:45 +0800

    fix: disable ESLint blocking build in Vercel

--- next.config.js
+++ next.config.js
@@ -1,8 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     eslint: {
-      ignoreDuringBuilds: true,
+      ignoreDuringBuilds: true, // ✅ 加上这个！
     },
+    // 其他配置可以继续放这
   }
   
   module.exports = nextConfig
